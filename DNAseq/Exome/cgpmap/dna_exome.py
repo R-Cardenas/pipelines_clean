7fde1c6ac01bb600a309971aaea28ed9fdfbc52e
R-Cardenas <R.Cardenas@uea.ac.uk>
2020-08-06 15:13:00 +0100

    swap os to subprocess

--- DNAseq/Exome/cgpmap/dna_exome.py
+++ DNAseq/Exome/cgpmap/dna_exome.py
@@ -9,11 +9,11 @@ from run_pipeline import data, home_dir
 # Select merge or no merge nextflow
 if data['merged_lanes'] == 'no':
     cgpmap_nf = "nextflow run dna-exome-nomerge.nf"
-    cmd = "cp dna-exome-nomerge.nf" + home_dir
+    cmd = "cp dna-exome-nomerge.nf " + home_dir
     os.system(cmd)
 elif data['merged_lanes'] == 'yes':
     cgpmap_nf = "nextflow run dna-exome-merge.nf"
-    cmd = "cp dna-exome-merge.nf" + home_dir
+    cmd = "cp dna-exome-merge.nf " + home_dir
     os.system(cmd)
 else:
     print('dna_exome.py - line10')
