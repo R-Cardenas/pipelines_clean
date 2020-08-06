bb4f1c4db6701ec5b3dccb8a3d344befcda8cdb2
R-Cardenas <R.Cardenas@uea.ac.uk>
2020-08-06 14:19:31 +0100

    swap os to subprocess

--- run_pipeline.py
+++ run_pipeline.py
@@ -12,16 +12,15 @@ except ImportError:
     from yaml import Loader, Dumper
 
 # Copy all nextflow to the workdir - as nextflow doesnt work...
-cp_nf = ['find','.','-name','"*.nf"','-exec','cp','{}','.','\;']
-print(cp_nf)
-subprocess.Popen(cp_nf, stdout=subprocess.PIPE ).communicate()[0]
+cp_nf = "find . -name '*.nf' -exec cp {} . \;"
+os.system(cp_nf)
 
 ## Create a bash script that will run all the pipelines selected by the config
-rm_existing = ["rm","-fr","run_selected_pipeline.sh"]
-new_sh = ["echo", "'#!/bin/bash'",">>","run_selected_pipeline.sh"]
+rm_existing = ["rm -fr run_selected_pipeline.sh"]
+new_sh = ["echo '#!/bin/bash' >> run_selected_pipeline.sh"]
 
-subprocess.Popen(rm_existing, stdout=subprocess.PIPE ).communicate()[0]
-subprocess.Popen(new_sh, stdout=subprocess.PIPE ).communicate()[0] ## should this be bsub? or lsf?
+os.system(rm_existing) ## should this be bsub? or lsf?
+os.system(new_sh)
 
 ## Open the user master yaml file
 with open('master_user_config.yaml') as f:
@@ -49,5 +48,5 @@ output = subprocess.Popen(run_master, stdout=subprocess.PIPE ).communicate()[0]
 print(output)
 
 ## Remove the nextflow files that were copied to keep clean
-cp_nf = ['rm','-fr','*.nf']
-subprocess.Popen(cp_nf, stdout=subprocess.PIPE ).communicate()[0]
+cp_nf = ['rm -fr *.nf']
+os.system(cp_nf)
