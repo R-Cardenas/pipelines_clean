1309185fcddab5ddfe083e9644bbe94896e5fbdd
R-Cardenas <R.Cardenas@uea.ac.uk>
2020-08-06 15:07:19 +0100

    swap os to subprocess

--- run_pipeline.py
+++ run_pipeline.py
@@ -12,7 +12,7 @@ except ImportError:
     from yaml import Loader, Dumper
 
 # Copy all nextflow to the workdir - as nextflow doesnt work...
-os.system("pwd")
+os.system("find . -name '*.nf' -exec cp {} .  \;")
 
 ## Create a bash script that will run all the pipelines selected by the config
 rm_existing = "rm -fr run_selected_pipeline.sh"
